d44fb792890dfb950b15e02d62cf5b56f71fbd60
wnsgur4322 <32208343+wnsgur4322@users.noreply.github.com>
2021-07-10 23:44:43 -0700

    connect with AWS amplify and winner function

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -33,6 +33,9 @@ class Board extends React.Component {
         }
         handleClick(i) {
                 const sqaures = this.state.sqaures.slice();
+                if (caculateWinner(squares) || squares[i]) {
+                        return;
+                }
                 sqaures[i] = this.state.xIsNext ? 'X' : 'O';
                 // sqaures[i] = 'X';
                 this.setState({
@@ -48,30 +51,37 @@ class Board extends React.Component {
         }
       
         render() {
-          const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      
-          return (
-            <div>
-              <div className="status">{status}</div>
-              <div className="board-row">
-                {this.renderSquare(0)}
-                {this.renderSquare(1)}
-                {this.renderSquare(2)}
-              </div>
-              <div className="board-row">
-                {this.renderSquare(3)}
-                {this.renderSquare(4)}
-                {this.renderSquare(5)}
-              </div>
-              <div className="board-row">
-                {this.renderSquare(6)}
-                {this.renderSquare(7)}
-                {this.renderSquare(8)}
-              </div>
-            </div>
-          );
+                const winner = caculateWinner(this.state.sqaures);
+                let status;
+                if (winner) {
+                        status = 'Winner: ' + winner;
+                } else {
+                        status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+                }
+          //const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        
+                return (
+                <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                        {this.renderSquare(0)}
+                        {this.renderSquare(1)}
+                        {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                        {this.renderSquare(3)}
+                        {this.renderSquare(4)}
+                        {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                        {this.renderSquare(6)}
+                        {this.renderSquare(7)}
+                        {this.renderSquare(8)}
+                </div>
+                </div>
+                );
         }
-      }
+}
       
 class Game extends React.Component {
         render() {
@@ -88,6 +98,26 @@ class Game extends React.Component {
           );
         }
       }
+
+function caculateWinner(squares) {
+        const  lines = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+                const [a, b, c] = lines[i];
+                if (squares[a] && squares[a] === sqaures[b] && squares[a] === squares[c]){
+                        return squares[a];
+                }
+        }
+        return null;
+}
       
       // ========================================
       
